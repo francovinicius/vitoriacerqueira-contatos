b4cf069c95ed086fd298dc163fede0d151f2375c
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-26 12:48:13 -0300

    alt about

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -6,22 +6,32 @@ export default function About() {
 
             <div className='div-apresentacao d-flex flex-column justify-content-center align-items-center' id='about'>
                 <p className='p-apresentacao text-center'>
-                    Olá! Meu nome é Vitória e sou uma psicóloga apaixonada por ajudar as pessoas a encontrarem seu bem-estar emocional e desenvolverem todo o seu potencial. Atuo na cidade de Volta Redonda, no Rio de Janeiro, onde tenho o privilégio de trabalhar em uma área que me permite impactar positivamente a vida das pessoas.
+                    Olá! Meu nome é Vitória, sou uma psicóloga e ajudar as pessoas a encontrarem seu bem-estar emocional e desenvolverem seu potencial é mais que uma missão profissional, mas pessoal também.
+
                 </p>
                 <p className='p-apresentacao text-center'>
-                    Com uma formação sólida em Psicologia e um profundo interesse pelo estudo da mente humana, estou sempre em busca de conhecimentos atualizados e técnicas eficazes para auxiliar meus clientes a lidarem com os desafios emocionais que enfrentam. Acredito que cada indivíduo é único e, por isso, ofereço um atendimento personalizado e acolhedor, adaptado às necessidades de cada pessoa que me procura.
+                    Atuo na cidade de Volta Redonda, no Rio de Janeiro e também atendo on-line, aqui tenho o privilégio de trabalhar em uma área que me permite impactar positivamente na vida de algumas pessoas
                 </p>
                 <p className='p-apresentacao text-center'>
-                    Além de minha formação acadêmica, também tenho experiência prática em diferentes áreas da psicologia, incluindo a terapia individual, de casal e de grupo. Trabalho com uma abordagem integrativa, combinando diversas técnicas terapêuticas, como a psicoterapia cognitivo-comportamental, a terapia humanista e a psicologia positiva. Essa abordagem multifacetada me permite oferecer uma perspectiva abrangente aos meus clientes, ajudando-os a alcançar uma maior compreensão de si mesmos e a desenvolver estratégias efetivas para lidar com os desafios emocionais que enfrentam.
+                    Com uma formação sólida em Psicologia pela Estácio de Sá e Pós Graduação em Análise do Comportamento em curso, além de profundo interesse pelo estudo da mente humana, estou sempre em busca de conhecimentos atualizados e técnicas eficazes para auxiliar meus clientes a lidarem com suas mazelas e encontrarem suas potencialidades.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Minha linha de atuação é a TCC (Teoria Cognitivo Comportamento) em que trabalhamos crenças, pensamentos e comportamentos.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Acredito que cada indivíduo é único e, por isso, ofereço um atendimento personalizado e acolhedor, adaptado às necessidades de cada pessoa que me procura.
                 </p>
                 <p className='p-apresentacao text-center'>
                     Valorizo a ética, a confidencialidade e o respeito mútuo em todas as minhas interações profissionais. Acredito que o ambiente terapêutico deve ser um espaço seguro e acolhedor, onde meus clientes possam se sentir à vontade para explorar suas emoções e compartilhar suas experiências de forma aberta e sincera.
                 </p>
                 <p className='p-apresentacao text-center'>
-                    Se você está passando por dificuldades emocionais, está enfrentando um momento de transição em sua vida ou simplesmente deseja investir em seu crescimento pessoal, estou aqui para ajudá-lo. Juntos, podemos traçar um caminho rumo ao seu bem-estar e à realização de seus objetivos.
+                    Se você está passando por dificuldades emocionais, está enfrentando um momento de transição em sua vida ou simplesmente deseja investir em seu crescimento pessoal, estou aqui para ajudá-lo.
+
                 </p>
                 <p className='p-apresentacao text-center'>
-                    Ficarei feliz em conhecê-lo(a) e auxiliá-lo(a) em sua jornada de autoconhecimento e transformação. Entre em contato e vamos começar essa jornada juntos!
+                    Juntos, podemos traçar um caminho rumo ao seu bem-estar e à realização de seus objetivos.
+                    Ficarei feliz em conhecê-lo(a) e auxiliá-lo(a) em sua jornada de autoconhecimento e transformação.
+                    Entre em contato e vamos começar essa jornada juntos!
                 </p>
             </div>
         </section>
